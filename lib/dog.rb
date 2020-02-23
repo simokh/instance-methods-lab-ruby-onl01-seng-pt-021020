b3742f98f4dd9c0fe8db992dcb8d8e7750fd3ead
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 19:25:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,15 +13,3 @@ puts "The Dog is sitting"
 end
 
 fido.sit
-
-# class Dog
-#   # Class body
-#
-#   # Instance Method Definition
-#   def bark
-#     puts "Woof!"
-#   end
-# end
-#
-# fido = Dog.new
-# fido.bark #> "Woof!"
