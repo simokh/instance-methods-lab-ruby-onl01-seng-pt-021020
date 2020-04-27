02881591aea3813780e07c8615e459cfefc38997
simokh <m.simokhouili@gmail.com>
2020-04-27 01:50:39 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
 class Dog
-  
-    def bark
-      puts "Woof!"
-    end
 
-    def sit
-      puts "The Dog is sitting"
-    end
+  def bark 
+    puts "Woof!"
+  end 
+
+  def sit 
+    puts "The Dog is sitting"
+  end 
 end 
 
-fido = Dog.new
+fido = Dog.new 
 fido.bark
-fido.sit
+fido.sit 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,14 @@
 class Person
 
-  def talk
+  def talk 
     puts "Hello World!"
-  end
+  end 
 
-  def walk
+  def walk 
     puts "The Person is walking"
-  end
+  end 
+end 
 
-end
-
-
-ada = Person.new
+ada = Person.new 
 ada.talk
 ada.walk
